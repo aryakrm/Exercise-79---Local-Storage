77373c31b770cee1de08b3daf3814d356dad6084
Arya Karimi <devaryakrm@gmal.com>
2022-10-19 21:42:48 +0300

    code added

---
+++ exercise.js
@@ -0,0 +1,14 @@
+const user = {
+  id: 1,
+  name: "John",
+  age: 25,
+};
+
+function setLs() {
+  JSON.stringify(user);
+  localStorage.setItem("id", `${user.id}`);
+  localStorage.setItem("name", `${user.name}`);
+  localStorage.setItem("age", `${user.age}`);
+}
+
+setLs();
